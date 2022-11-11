453fae98ba864f52a4c3979f4c3b0ca196a00008
Vivek Mishra <57038169+vm7151810@users.noreply.github.com>
2022-11-11 22:33:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var log = document.getElementById("log") ;
 var reg = document.getElementById("reg") ;
-var btn = document.getElementById("btn") ;
+var botn = document.getElementById("btn") ;
 
 function register() {
     log.style.left = "-400px" ;   
     reg.style.left = "90px" ;
-    btn.style.left = "110px" ;
+    botn.style.left = "110px" ;
 }
 
 function login() {
     log.style.left = "90px" ;   
     reg.style.left = "490px" ;
-    btn.style.left = "0px" ;
+    botn.style.left = "0px" ;
 }
 
 function check(input) {
